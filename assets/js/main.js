550adf6f260c458e33960c93855e95571319c26b
Aidas Bendoraitis <aidasbend@yahoo.com>
2015-08-29 04:08:15 +0200

    restarting game without the splash screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 cc.game.onStart = function(){
     //load resources
     cc.view.setDesignResolutionSize(640, 480, cc.ResolutionPolicy.SHOW_ALL);
-
     cc.LoaderScene.preload(gameResources, function () {
         cc.director.runScene(new GameScene());
     }, this);
 };
+cc.game.onRestart = function(){
+    cc.view.setDesignResolutionSize(640, 480, cc.ResolutionPolicy.SHOW_ALL);
+    cc.director.runScene(new GameScene());
+};
+cc.game.restart = function () {
+    cc.director.popToSceneStackLevel(0);
+    cc.audioEngine && cc.audioEngine.end();
+    cc.game.onRestart();
+}
+
 cc.game.run();
